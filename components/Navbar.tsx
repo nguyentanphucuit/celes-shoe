194f775323da3b9c56a7344213ba5eccd0194bb3
Peter Nguyen <peter.nguyen@siliconstack.com.au>
2023-10-12 22:13:25 +0700

    update navbar avt

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,21 @@
 "use client";
 import Image from "next/legacy/image";
 import Link from "next/link";
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Banner from "./Banner";
 import CartModal from "./CartModal";
+import { useRouter } from "next/router";
+import { Popover, Transition } from "@headlessui/react";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [showProfile, setShowProfile] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setShowMenu(false);
+    setShowProfile(false);
+  }, [router.asPath]);
 
   const handleOpenMenu = () => {
     setShowMenu(!showMenu);
@@ -91,7 +99,45 @@ const Navbar = () => {
               <CartModal />
 
               <div className="hidden md:block relative ml-3 px-2">
-                <button
+                <Popover className="relative">
+                  {() => (
+                    <>
+                      <Popover.Button>
+                        <Image
+                          width={32}
+                          height={32}
+                          src="/profile.png"
+                          className="rounded-full"
+                          style={{ height: "32px" }}
+                          alt="avatar"
+                          onClick={handleOpenProfile}
+                        />
+                      </Popover.Button>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="opacity-0 translate-y-1"
+                        enterTo="opacity-100 translate-y-0"
+                        leave="transition ease-in duration-150"
+                        leaveFrom="opacity-100 translate-y-0"
+                        leaveTo="opacity-0 translate-y-1">
+                        <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-32 max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+                          <div className="overflow-hidden bg-white rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                            {profileItem.map((item, index) => (
+                              <Link
+                                key={index}
+                                href={item.path}
+                                className="profile-dropdown">
+                                {item.title}
+                              </Link>
+                            ))}
+                          </div>
+                        </Popover.Panel>
+                      </Transition>
+                    </>
+                  )}
+                </Popover>
+                {/* <button
                   type="button"
                   className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <Image
@@ -121,7 +167,7 @@ const Navbar = () => {
                       </Link>
                     ))}
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
