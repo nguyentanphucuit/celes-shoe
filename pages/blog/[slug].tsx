1086ecaf14013297df341f1eeaea5048f138792b
Peter Nguyen <peter.nguyen@siliconstack.com.au>
2023-10-14 03:37:47 +0700

    fix error slug (undefined || null)

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -9,7 +9,8 @@ import { useAppSelector } from "@/redux/hooks";
 const Blog = () => {
   const router = useRouter();
   const data = useAppSelector((state) => state.blogReducer.items);
-  const mainData = data.find((blog) => blog.slug == router.query.slug);
+  const mainData =
+    data.find((blog) => blog.slug == router.query.slug) ?? data[0];
   return (
     <div className="my-20 lg:mt-40 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
